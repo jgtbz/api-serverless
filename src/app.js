4981083b63eb542a984fa3244d71e5c3516d883e
João <joao@ecobonuz.com>
2021-05-16 10:54:31 -0300

    att

--- src/app.js
+++ src/app.js
@@ -12,13 +12,9 @@ const sendSNS = async ({ topic, message }) => {
     TopicArn
   }
 
-  console.log({
-    config,
-    params
-  })
-
   const data = await new AWS.SNS().publish(params).promise()
-  console.log(data)
+
+  return data?.MessageId
 }
 
 export {
